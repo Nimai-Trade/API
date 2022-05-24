1b400cd4899795c86d2d651c50c4ccf662ee457e
adil bhati <adil.bhati@cloverinfotech.com>
2022-05-24 12:28:44 +0530

    RXIL changes

--- spring-boot-jwt/src/main/java/com/javainuse/config/JwtTokenUtil.java
+++ spring-boot-jwt/src/main/java/com/javainuse/config/JwtTokenUtil.java
@@ -1,6 +1,12 @@
 package com.javainuse.config;
 
+import java.io.ByteArrayInputStream;
 import java.io.Serializable;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -8,7 +14,12 @@ import java.util.Map;
 import java.util.function.Function;
 
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.keys.RsaKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -23,7 +34,7 @@ import com.javainuse.repository.ReferRepository;
 import com.javainuse.repository.ReferralLeadsRepo;
 import com.javainuse.bean.ReferBean;
 import com.javainuse.utility.ReferenceIdUniqueNumber;
-
+import com.mysql.cj.util.Base64Decoder;
 import com.javainuse.model.NimaiCustomer;
 import com.javainuse.model.Refer;
 
@@ -96,18 +107,56 @@ public class JwtTokenUtil implements Serializable {
 
 	public ReferralLeads retrieveClaims(String token,String referType) throws Exception
 	{
-		Date d=new Date();
+		Claims body = null;
+		
 		byte[] key;
 		if(referType.equalsIgnoreCase("fieo"))
+		{
 			key=fieoSecret;
+			body = Jwts.parser()
+					.setSigningKey(key)
+	                //.setSigningKey(fieoSecret)
+	                .parseClaimsJws(token)
+	                .getBody();
+			return retrieveFIEOClaims(body,referType);
+		}
 		else
+		{
 			key=rxilSecret;
-		Claims body = Jwts.parser()
-				.setSigningKey(key)
-                //.setSigningKey(fieoSecret)
-                .parseClaimsJws(token)
-                .getBody();
+			
+			
+			Base64Decoder base64Decoder = new Base64Decoder();
+	        byte[] publicKeyBytes = base64Decoder.decode(key, 0, key.length);
+	        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+	        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+	        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+	        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+	                //.setRequireExpirationTime()
+	                .setVerificationKey(publicKey)
+	                .build();
+	        JwtClaims jwtDecoded = jwtConsumer.processToClaims(token);
+	        Map<String, Object> jwtClaims = jwtDecoded.getClaimsMap();
+	        String username = (String) jwtClaims.get("pb_referral_code"); // "MChambe4"
+	        System.out.println("jwtDecoded: "+jwtDecoded.toString());
+	        
+	        System.out.println("Claims: "+jwtClaims);
+	        System.out.println("Referral code: "+username);
+	        return retrieveRXILClaims(jwtClaims, referType);
+	        
+		}
 		
+		
+		
+		
+		//System.out.println("EmailId: "+body.get("email"));
+		//System.out.println("Mobile no: "+body.get("mobile"));
+		//System.out.println("All claims: "+getAllClaimsFromToken(token));
+		
+	}
+	
+	private ReferralLeads retrieveRXILClaims(Map<String, Object> body, String referType) {
+		// TODO Auto-generated method stub
+		Date d=new Date();
 		System.out.println("Claims: "+body.toString());
 		String firstName="",lastName="";
 		String[] userName=null;
@@ -250,11 +299,152 @@ public class JwtTokenUtil implements Serializable {
 			return updateRl;
 		}
 		
-		
-		//System.out.println("EmailId: "+body.get("email"));
-		//System.out.println("Mobile no: "+body.get("mobile"));
-		//System.out.println("All claims: "+getAllClaimsFromToken(token));
-		
+	}
+
+	public ReferralLeads retrieveFIEOClaims(Claims body,String referType)
+	{
+		Date d=new Date();
+		System.out.println("Claims: "+body.toString());
+		String firstName="",lastName="";
+		String[] userName=null;
+		if(body.get("pb_name")==null)
+		{
+			firstName="";
+			lastName="";
+		}
+		else
+		{
+			userName=body.get("pb_name").toString().split(" ");
+			if(userName.length>2)
+			{
+				firstName=userName[0]+" "+userName[1];
+				lastName=userName[2];
+			}
+			else
+			{
+				firstName=userName[0];
+				try
+				{
+					lastName=userName[1];
+				}
+				catch(Exception e)
+				{
+					lastName="";
+				}
+			}
+		}
+		System.out.println("First name: "+firstName);
+		System.out.println("Last name: "+lastName);
+		String mobNo="";
+		if(body.get("pb_mobile")==null)
+			mobNo="";
+		else
+			mobNo=body.get("pb_mobile").toString().replace(" ", "");
+		String upToNCharacters = mobNo.substring(0, Math.min(mobNo.length(), 3));
+		System.out.println("Mobile 3 char: "+upToNCharacters);
+		if(!upToNCharacters.contains("+91"))
+			mobNo=mobNo;
+		else
+			mobNo=mobNo.substring(3);
+		System.out.println("Mobile no: "+mobNo);
+		String emailID="";
+		if(body.get("pb_email")==null)
+			emailID="";
+		else
+			emailID=body.get("pb_email").toString().toLowerCase();
+		ReferralLeads rl=new ReferralLeads();
+		ReferralLeads updateRl;
+		if(body.get("pb_organization")==null)
+			updateRl=refRepo.getRlDetails(emailID,mobNo,"");
+		else
+			updateRl=refRepo.getRlDetails(emailID,mobNo,body.get("pb_organization").toString());
+		System.out.println("ReferralLeads: "+updateRl);
+		if(updateRl==null ) {
+			System.out.println("Saving Referral leads....");
+			rl.setpId(""+body.get("pb_p_id"));
+			if(referType.equalsIgnoreCase("fieo"))
+				rl.setReferralId("FIEO");
+			else
+				rl.setReferralId("RXIL");
+			//rl.setReferBy(fieoRefId);
+			rl.setReferBy(""+body.get("pb_referral_code"));
+			rl.setFirstName(""+firstName);
+			rl.setLastName(""+lastName);
+			rl.setOrgName(""+body.get("pb_organization"));
+			rl.setDesignation(""+body.get("pb_designation"));
+			rl.setEmailId(""+emailID);
+			rl.setMobileNo(mobNo);
+			//rl.setLandline(""+body.get("landline"));
+			rl.setAddress(""+body.get("pb_street"));
+			rl.setCity(""+body.get("pb_city"));
+			rl.setState(""+body.get("pb_state"));
+			if(body.get("pb_country")==null)
+				rl.setCountry("INDIA");
+			else
+				rl.setCountry(""+body.get("pb_country").toString().toUpperCase());
+			rl.setPincode(""+body.get("pb_pincode"));
+			//rl.setMembershipStatus("Paid Member");
+			if(referType.equalsIgnoreCase("rxil"))
+			{
+				if(body.get("pb_membership").toString().equalsIgnoreCase("NonMember"))
+					rl.setMembershipStatus(""+body.get("pb_membership"));
+				else
+					rl.setMembershipStatus("Member");
+			}
+			else
+				rl.setMembershipStatus(""+body.get("pb_membership"));
+			rl.setRedirectUrl(""+body.get("pb_redirecturl"));
+			rl.setIec(""+body.get("pb_iec"));
+			rl.setFax(""+body.get("pb_fax"));
+			rl.setGstin(""+body.get("pb_gstin"));
+			rl.setInsertedBy(""+body.get("pb_name"));
+			rl.setInsertedDate(d);
+			rl.setModifiedBy(""+body.get("pb_name"));
+			rl.setModifiedDate(d);
+			rl.setJsonBody(body.toString());
+			refRepo.save(rl);
+			saveReferData("save",rl);
+			return rl;
+		}else {
+			updateRl=refRepo.getOne(updateRl.getLeadId());
+			updateRl.setpId(""+body.get("pb_p_id"));
+			if(referType.equalsIgnoreCase("fieo"))
+				updateRl.setReferralId("FIEO");
+			else
+				updateRl.setReferralId("RXIL");
+			//updateRl.setReferBy(fieoRefId);
+			updateRl.setReferBy(""+body.get("pb_referral_code"));
+			updateRl.setFirstName(""+firstName);
+			updateRl.setLastName(""+lastName);
+			updateRl.setOrgName(""+body.get("pb_organization"));
+			updateRl.setDesignation(""+body.get("pb_designation"));
+			updateRl.setEmailId(""+emailID);
+			
+			updateRl.setMobileNo(mobNo);
+			//rl.setLandline(""+body.get("landline"));
+			updateRl.setAddress(""+body.get("pb_street"));
+			updateRl.setCity(""+body.get("pb_city"));
+			updateRl.setState(""+body.get("pb_state"));
+			if(body.get("pb_country")==null)
+				updateRl.setCountry("INDIA");
+			else
+				updateRl.setCountry(""+body.get("pb_country").toString().toUpperCase());
+			updateRl.setPincode(""+body.get("pb_pincode"));
+			//rl.setMembershipStatus("Paid Member");
+			updateRl.setMembershipStatus(""+body.get("pb_membership"));
+			updateRl.setRedirectUrl(""+body.get("pb_redirecturl"));
+			updateRl.setIec(""+body.get("pb_iec"));
+			updateRl.setFax(""+body.get("pb_fax"));
+			updateRl.setGstin(""+body.get("pb_gstin"));
+			//updateRl.setInsertedBy(""+body.get("pb_name"));
+			//updateRl.setInsertedDate(d);
+			updateRl.setModifiedBy(""+body.get("pb_name"));
+			updateRl.setModifiedDate(d);
+			updateRl.setJsonBody(body.toString());
+			refRepo.save(updateRl);
+			saveReferData("update",updateRl);
+			return updateRl;
+		}
 	}
 	
 	private void saveReferData(String flag, ReferralLeads rl) {
